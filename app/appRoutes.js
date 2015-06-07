d310425cda6d1e9c79b5923cc2ca0e77bc8d5a33
GarethFentimen <garethfentimen@gmail.com>
2015-06-07 20:44:32 +0100

    started work on the request quote form

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -10,6 +10,21 @@ module.exports = function(router) {
         var data = { title: 'Kieron Williams AutoAllSmart' };
         res.render('home', data);
     });
+
+    router.post('/quote', function(req, res) {
+        // Require
+        var postmark = require("postmark");
+
+        // Example request
+        var client = new postmark.Client("2949cc1f-72cb-438d-b166-95bf2d9fd823");
+
+        client.sendEmail({
+            "From": "mail@kwautoallsmart.com",
+            "To": "garethfentimen@gmail.com",
+            "Subject": "Test", 
+            "TextBody": "Hello from Postmark!"
+        });
+    });
     
     router.get('/contact', function(req, res) {
         var data = { title: 'Kieron Williams AutoAllSmart' };
